c30c6619ccc358681b5c71105e16710ba6569451
Packingdustry <alois.mc@hotmail.com>
2024-11-19 17:28:10 +0100

    ajoute la classe Personne {id, nom, prenom}

---
+++ src/main/java/org/example/Personne.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Personne {
+    private int id;
+    private String nom, prenom;
+
+    public Personne(String nom, String prenom) {
+        this.id = -1;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
