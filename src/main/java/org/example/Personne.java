f64e0be6a36d6e707a5dac18738296be906a65b8
Packingdustry <alois.mc@hotmail.com>
2024-11-22 15:37:55 +0100

    7.1. ajoute la méthode findAll() et un toString()

--- src/main/java/org/example/Personne.java
+++ src/main/java/org/example/Personne.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Personne {
     private int id;
     private String nom, prenom;
@@ -8,5 +12,32 @@ public class Personne {
         this.id = -1;
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static List<Personne> findAll() throws ClassNotFoundException, SQLException {
+        Connection connect = DBConnection.getConnection();
+
+        String SQLPrep = "SELECT * FROM personne;";
+        PreparedStatement prep1 = connect.prepareStatement(SQLPrep);
+        prep1.execute();
+        ResultSet rs = prep1.getResultSet();
+        ArrayList<Personne> personnes = new ArrayList<>();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String nom = rs.getString("nom");
+            String prenom = rs.getString("prenom");
+            Personne p = new Personne(nom, prenom);
+            p.setId(id);
+            personnes.add(p);
+        }
+        return personnes;
+    }
+
+    public String toString() {
+        return "-> (" + id + ") " + nom + ", " + prenom;
     }
 }
